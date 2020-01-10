810ffea46468e60071a0d857d0b0ed03fe95e96c
Federica Zuccarello <federicucciaaa_95@hotmail.it>
2020-01-10 12:28:34 +0100

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -11,11 +11,6 @@ import javax.swing.border.Border;
 import org.json.JSONObject;
 import server.linkedin.LinkedinSuccess;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
